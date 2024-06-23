d79984be5505e87965cecf9a0202ca330c59f3d1
GabrielSpdf <gb.spada@hotmail.com>
2024-06-23 19:35:21 -0300

    funcao troca de cor funcionando

--- heart_code/apolo_heart/apolo_heart.ino
+++ heart_code/apolo_heart/apolo_heart.ino
@@ -1,250 +1,118 @@
-#include "Adafruit_NeoPixel.h"
-#include <ros.h>
-#include <std_msgs/UInt16.h>
-#include "std_msgs/String.h"
- 
+#include "Adafruit_NeoPixel.h"  // Library for controlling NeoPixel LEDs
+#include <ros.h>                // ROS (Robot Operating System) library
+#include <std_msgs/UInt16.h>    // ROS standard message library for UInt16
+#include "std_msgs/String.h"    // ROS standard message library for String
+
+// Define constants
+#define LED_PIN  5 			  // Digital pin connected to "DIN" of WS2812 module
+#define NUM_LEDS 16 		  // Number of LEDs in the WS2812 module
+#define BRIGHTNESS 255 		// LED brightness (0 to 255)
+
+// Create a ROS string message
+std_msgs::String str_msg;
+
+// Create a ROS publisher
+ros::Publisher chatter("chatter", &str_msg);
+
+// Create a ROS node handle
 ros::NodeHandle  nh;
 
-#define LED_PIN  5 			// Pino digital do "DIN" do módulo WS2812 
-#define NUM_LEDS 16 		// Número de Leds do módulo Led WS2812
-#define BRIGHTNESS 255 		// Intensidade do LED (0 até 255) 
+// Variable to identify the position of the LED in the ring (0 to 15)
+int i;                                         
 
-int i;                                         // Variável que identifica a posição do LED no anel (0 até 15)   
-String emotion_name;  
-
+// Create an instance of Adafruit_NeoPixel for controlling the LEDs
 Adafruit_NeoPixel leds(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-enum Color {
-    SERENITY,
-    JOY,
-    ACCEPTANCE,
-    TRUST,
-    ECSTASY,
-    ADMIRATION,
-    TERROR,
-    FEAR,
-    APPREHENSION,
-    AMAZEMENT,
-    SURPRISE,
-    DISTRACTION,
-    PENSIVENESS,
-    SADNESS,
-    BOREDOM,
-    DISGUST,
-    GRIEF,
-    ANNOYANCE,
-    ANGER,
-    RAGE,
-    INTEREST,
-    ANTICIPATION,
-    VIGILANCE,
-    OPTIMISM,
-    LOVE,
-    SUBMISSION,
-    AWE,
-    DISAPPROVAL,
-    REMORSE,
-    CONTEMPT,
-    AGGRESSIVENESS,
-    NUM_COLORS
+// Structure to store RGB values and associated emotion
+struct RGB{
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	const char* emotion;
 };
 
-struct RGB {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-};
+// Number of colors defined
+const int NUM_COLORS = 24;
 
+// Array to map colors to emotions
 RGB colorMap[NUM_COLORS] = {
-    {255, 223, 186},  // SERENITY
-    {255, 255, 85},   // JOY
-    {119, 221, 119},  // ACCEPTANCE
-    {17, 170, 42},    // TRUST
-    {255, 215, 0},    // ECSTASY
-    {255, 165, 0},    // ADMIRATION
-    {0, 255, 255},    // TERROR
-    {0, 255, 127},    // FEAR
-    {0, 139, 69},     // APPREHENSION
-    {0, 255, 255},    // AMAZEMENT
-    {0, 191, 255},    // SURPRISE
-    {224, 255, 255},  // DISTRACTION
-    {70, 130, 180},   // PENSIVENESS
-    {0, 0, 255},      // SADNESS
-    {139, 0, 139},    // BOREDOM
-    {128, 0, 128},    // DISGUST
-    {255, 0, 255},    // GRIEF
-    {255, 140, 0},    // ANNOYANCE
-    {255, 0, 0},      // ANGER
-    {139, 0, 0},      // RAGE
-    {255, 140, 0},    // INTEREST
-    {255, 69, 0},     // ANTICIPATION
-    {255, 0, 0},      // VIGILANCE
-    {0, 0, 0},        //OPTIMISM
-    {0, 0, 0},        //LOVE
-    {0, 0, 0},        //SUBMISSION
-    {0, 0, 0},        //AWE
-    {0, 0, 0},        //DISAPPROVAL
-    {0, 0, 0},        //REMORSE
-    {0, 0, 0},        //CONTEMPT
-    {0, 0, 0}         //AGGRESSIVENESS
+	{255, 255, 51, "serenity"},
+	{230, 230, 30, "joy"},
+	{204, 204, 0, "ecstasy"},
+	{119, 221, 119, "acceptance"},
+	{17, 170, 42, "trust"},
+	{102, 204, 0, "admiration"},
+	{102, 102, 0, "apprehension"},
+	{76, 153, 0, "fear"},
+	{0, 255, 0, "terror"},
+	{153, 204, 255, "distraction"},
+	{51, 153, 255, "surprise"},
+	{0, 102, 204, "amazement"},
+	{102, 102, 255, "pensiveness"},
+	{0, 0, 255, "sadness"},
+	{0, 0, 153, "grief"},
+	{255, 51, 255, "boredom"},
+	{204, 0, 204, "disgust"},
+	{102, 0, 102, "loathing"},
+	{255, 102, 102, "annoyance"},
+	{255, 51, 51, "anger"},
+	{255, 0, 0, "rage"},
+	{255, 215, 0, "interest"},
+	{255, 165, 0, "anticipation"},
+	{153, 76, 0, "vigilance"}
 };
 
-void callback(){}
+// Function to change the LED color based on the emotion
+void change_color(const std_msgs::String& msg){
+	for(int i = 0; i < NUM_COLORS; i++){
+		if(strcmp(msg.data, colorMap[i].emotion) == 0){
+			uint8_t r = colorMap[i].r;
+			uint8_t g = colorMap[i].g;
+			uint8_t b = colorMap[i].b;
+      
 
-//ros::Subscriber sub = nh.subscribe("/utbots/display_emotions/emotion", 1000);
-//ros::Subscriber<std_msgs::UInt16> face_sub();
-
-void change_color(int color){
-    for(i = 0; i < NUM_LEDS; i++) {   
-      leds.setPixelColor(i, leds.Color(colorMap[color].r,colorMap[color].g,colorMap[color].b));
-    } 
-    leds.show(); 
+      // Set the color for each LED in the ring
+			for (int j = 0; j < NUM_LEDS; j++){
+				leds.setPixelColor(j, leds.Color(r, g, b));
+			}
+      
+      delay(3200);
+			leds.show();
+			delay(2000);
+			return;
+		}
+	}
 }
 
-// callback to read emotion request
-void callback(const std_msgs::String &msg)
-{
-  emotion_name = msg.data;
-  Serial.println(emotion_name);
-
-  if (emotion_name == "annoyance")
-  {
-  }
-  else if (emotion_name == "much_annoyance")
-  {
-  }
-  else if (emotion_name == "anger")
-  {
-  }
-  else if (emotion_name == "rage")
-  {
-  }
-  else if (emotion_name == "interest")
-  {
-  }
-  else if (emotion_name == "much_interest")
-  {
-  }
-  else if (emotion_name == "anticipation")
-  {
-  }
-  else if (emotion_name == "vigilance")
-  {
-  }
-  else if (emotion_name == "boredom")
-  {
-  }
-  else if (emotion_name == "much_boredom")
-  {
-  }
-  else if (emotion_name == "disgust")
-  {
-  }
-  else if (emotion_name == "loathing")
-  {
-  }
-  else if (emotion_name == "apprehension")
-  {
-  }
-  else if (emotion_name == "much_apprehension")
-  {
-  }
-  else if (emotion_name == "fear")
-  {
-  }
-  else if (emotion_name == "terror")
-  {
-  }
-  else if (emotion_name == "serenity")
-  {
-  }
-  else if (emotion_name == "much_serenity")
-  {
-  }
-  else if (emotion_name == "joy")
-  {
-    for(i = 0; i < NUM_LEDS; i++) {   
-      leds.setPixelColor(i, leds.Color(colorMap[JOY].r,colorMap[JOY].g,colorMap[JOY].b));
-    } 
-    leds.show(); 
-    delay(1000);
-  }
-  else if (emotion_name == "ecstasy")
-  {
-  }
-  else if (emotion_name == "pensiveness")
-  {
-  }
-  else if (emotion_name == "much_pensiveness")
-  {
-  }
-  else if (emotion_name == "sadness")
-  {
-  }
-  else if (emotion_name == "grief")
-  {
-  }
-  else if (emotion_name == "distraction")
-  {
-  }
-  else if (emotion_name == "much_distraction")
-  {
-  }
-  else if (emotion_name == "surprise")
-  {
-  }
-  else if (emotion_name == "amazement")
-  {
-  }
-  else if (emotion_name == "acceptance")
-  {
-  }
-  else if (emotion_name == "much_acceptance")
-  {
-  }
-  else if (emotion_name == "trust")
-  {
-    for(i = 0; i < NUM_LEDS; i++) {   
-      leds.setPixelColor(i, leds.Color(colorMap[TRUST].r,colorMap[TRUST].g,colorMap[TRUST].b));
-    } 
-    leds.show(); 
-    delay(1000);
-  }
-  else if (emotion_name == "admiration")
-  {
-  }
-  else if (emotion_name == "idle")
-  {
-  }
-  else{
-
-  }
+// Callback function for the ROS subscriber
+void callback(const std_msgs::String& msg)
+{	
+	change_color(msg);
 }
 
-void setup() {
-  Serial.begin(57600);
-  nh.initNode();
-  ros::Subscriber<std_msgs::String> sub("/utbots/display_emotions/emotion", callback);
-  nh.subscribe(sub);
-  leds.begin(); 
-  leds.clear(); 
+void setup(){
+	Serial.begin(57600);   // Start serial communication
+	nh.initNode();         // Initialize ROS node
+
+  // Create a ROS subscriber to listen for emotion messages
+	ros::Subscriber<std_msgs::String> sub("/utbots/display_emotions/emotion", callback);
+
+	nh.subscribe(sub);   // Subscribe to the topic
+	leds.begin();        // Initialize the LED strip
+	leds.clear();        // Clear any previous LED colors
 }
 
-void loop() {
-  /*for(int color = SERENITY; color < NUM_COLORS; color++){ 
-    for(i = 0; i < NUM_LEDS; i++) {   
-      leds.setPixelColor(i, leds.Color(colorMap[color].r,colorMap[color].g,colorMap[color].b));
-    } 
-    leds.show();   
-    delay(10000);
-  }*/
-  for(i = 0; i < NUM_LEDS; i++) {   
-      leds.setPixelColor(i, leds.Color(10, 10, 10));
-  }
+void loop(){
+  // Set a default color for each LED in the ring
+	for(i = 0; i < NUM_LEDS; i++){   
+		leds.setPixelColor(i, leds.Color(10, 10, 10));
+	}
 
-  leds.show();   
+	leds.show();      // Update the LED colors
 
-  nh.spinOnce();
-  delay(100);
-} 
-  
+	nh.spinOnce();    // Handle ROS communication
+	delay(100);       // Wait for 100 milliseconds
+
+  //change_color("rage");
+  //delay(2000);
+}
